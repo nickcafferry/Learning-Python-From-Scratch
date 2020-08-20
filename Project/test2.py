b4d15c5584e13b96a4a076dbc37f30012da785fc
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-08-21 01:10:34 +0800

    Create test2.py

---
+++ Project/test2.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+import os
+import time
+import webbrowser as web
+for i in range(1000):
+	web.open("https://sherman.fe.liulishuo.com/dubbing?shareId=1597934420:4a40e7e1bed498ed")
+	time.sleep(3)
